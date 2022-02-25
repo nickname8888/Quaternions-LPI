9fa12c792e0e6f6a3c44227b473d1327e3eeef95
Parmesh Walunj <61390825+parmeshwalunj@users.noreply.github.com>
2022-02-25 13:08:22 +0530

    Update desk_app.py

--- desk_app.py
+++ desk_app.py
@@ -1,29 +1,28 @@
+import tkinter
 from tkinter import *
 
+W = tkinter.Tk()
+W.geometry("1280x720+0+0")
+W.configure(background="lightblue")
 c = 0
 
+
 def exec_code():
-    print("Running code...")
     global c
     c = c+1
-    print(c)
+    label1["text"] = c
 
-root = Tk()
-root.geometry("1400x700")
 
-frame1 = Frame(root)
-frame2 = Frame(root)
-root.title("tkinter frame")
+label1 = Label(W, text="Click the Start Button to update")
+label1.place(relx=0.0,
+             rely=1.0,
+             anchor='sw')
+frame = Frame(W, width=1260, height=620,
+              highlightbackground="black", highlightthickness=1)
+frame.place(x=10, y=10)
+label = Label(frame, text="Video").pack()
+frame.pack_propagate(False)
+b1 = Button(W, text="Start", command=exec_code, pady=10).place(x=570, y=650)
+b2 = Button(W, text="Stop", pady=10).place(x=650, y=650)
 
-label = Label(frame1, text="Video", justify=LEFT)
-label.pack(side=LEFT)
-
-# hi_there = Button(frame2, text="say hi~", command=say_hi)
-# hi_there.pack()
-b1 = Button(root, text="Start", command=exec_code, pady=10).place(x=640, y=600)
-b2 = Button(root, text="Stop", pady=10).place(x=710, y=600)
-
-frame1.pack(padx=1, pady=1)
-frame2.pack(padx=10, pady=10)
-
-root.mainloop()
+W.mainloop()
